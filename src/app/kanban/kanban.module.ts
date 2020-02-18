8b4c38bd65a90e1cd5b107a4c8716871233a49c0
Puppy Pc <freyanoir@gmail.com>
2020-02-18 20:22:12 +0200

    kanban routing added

---
+++ src/app/kanban/kanban.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { KanbanRoutingModule } from './kanban-routing.module';
+import { BoardListComponent } from './board-list/board-list.component';
+
+
+@NgModule({
+  declarations: [BoardListComponent],
+  imports: [
+    CommonModule,
+    KanbanRoutingModule,
+    SharedModule,
+    FormsModule,
+    DragDropModule,
+    MatDialogModule,
+    MatButtonToggleModule,
+  ]
+})
+export class KanbanModule { }
